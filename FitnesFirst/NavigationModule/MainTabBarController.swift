dd8879b6e9035165a6f38d965f0c330b089d040c
Dmitrii642 <dmit.iakovlev@gmail.com>
2023-10-04 17:50:29 +0300

    Add: TimerViewController

--- FitnesFirst/NavigationModule/MainTabBarController.swift
+++ FitnesFirst/NavigationModule/MainTabBarController.swift
@@ -5,40 +5,40 @@
 //  Created by Дмитрий Яковлев on 18.09.2023.
 //
 
-import UIKit
+    import UIKit
 
-class MainTabBarController: UITabBarController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    class MainTabBarController: UITabBarController {
         
-        setupTabBar()
-        setupItems()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            setupTabBar()
+            setupItems()
+        }
+        
+        private func setupTabBar() {
+            tabBar.backgroundColor = .specialTabBar
+            tabBar.tintColor = .specialDarkGreen
+            tabBar.unselectedItemTintColor = .specialGray
+            tabBar.layer.borderWidth = 1
+            tabBar.layer.borderColor = UIColor.specialLightBrown.cgColor
+        }
+        
+        private func setupItems() {
+            
+            let mainVC = MainViewController()
+            let ststisticVC = StatisticViewController()
+            
+            setViewControllers([mainVC, ststisticVC], animated: true)
+            
+            guard let items = tabBar.items else { return }
+            items[0].title = "Main"
+            items[1].title = "Statistic"
+            
+            items[0].image = UIImage(named: "mainTabBar")
+            items[1].image = UIImage(named: "statisticTabBar")
+            
+            UITabBarItem.appearance().setTitleTextAttributes([.font: UIFont(name: "Roboto-Bold", size: 12) as Any], for: .normal)
+        }
+        
     }
-    
-    private func setupTabBar() {
-        tabBar.backgroundColor = .specialTabBar
-        tabBar.tintColor = .specialDarkGreen
-        tabBar.unselectedItemTintColor = .specialGray
-        tabBar.layer.borderWidth = 1
-        tabBar.layer.borderColor = UIColor.specialLightBrown.cgColor
-    }
-    
-    private func setupItems() {
-        
-        let mainVC = MainViewController()
-        let ststisticVC = StatisticViewController()
-        
-        setViewControllers([mainVC, ststisticVC], animated: true)
-        
-        guard let items = tabBar.items else { return }
-        items[0].title = "Main"
-        items[1].title = "Statistic"
-        
-        items[0].image = UIImage(named: "mainTabBar")
-        items[1].image = UIImage(named: "statisticTabBar")
-        
-        UITabBarItem.appearance().setTitleTextAttributes([.font: UIFont(name: "Roboto-Bold", size: 12) as Any], for: .normal)
-    }
-    
-}
